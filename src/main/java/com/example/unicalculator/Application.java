fffa275cb51463f8ae968ee24c32d91a5aa54033
CheAlOwn <chealo890@gmail.com>
2024-04-20 20:50:09 +0300

    start calc task

--- src/main/java/com/example/unicalculator/Application.java
+++ src/main/java/com/example/unicalculator/Application.java
@@ -1,18 +1,16 @@
 package com.example.unicalculator;
 
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("filesFXML/mainPage-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("filesFXML/mainPage-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 450, 700);
-        scene.getStylesheets().add("resources/com/example/unicalculator/CSS/styleMainPage.CSS");
 
         stage.setTitle("UniCalculator");
         stage.setScene(scene);
