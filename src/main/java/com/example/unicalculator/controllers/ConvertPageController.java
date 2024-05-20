b2144220dad05dc0641a7134e98fcd77d0830142
Quark <zuchok12@mail.ru>
2024-05-20 20:18:53 +0300

    Calculator logic

--- src/main/java/com/example/unicalculator/controllers/ConvertPageController.java
+++ src/main/java/com/example/unicalculator/controllers/ConvertPageController.java
@@ -1,41 +1,30 @@
 package com.example.unicalculator.controllers;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class ConvertPageController {
 
-    @FXML
-    private Button ageBut;
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
 
     @FXML
-    private Button calcButton;
-
-    @FXML
-    private Button categoriesBut;
-
-    @FXML
-    private Button convertBut;
+    private Button ageBut, calcButton, categoriesBut, convertBut, currencyBut, dataBut, dateBut, discountBut, lengthBut
+            , timeBut, velocityBut, volumeBut, weightBut;
 
     @FXML
     private AnchorPane convertWindow;
-
-    @FXML
-    private Button currencyBut;
-
-    @FXML
-    private Button dataBut;
-
-    @FXML
-    private Button dateBut;
-
-    @FXML
-    private Button discountBut;
-
-    @FXML
-    private Button lengthBut;
 
     @FXML
     private FlowPane mainBar;
@@ -44,15 +33,34 @@ public class ConvertPageController {
     private FlowPane numSysBut;
 
     @FXML
-    private Button timeBut;
+    private void initialize() {
+        calcButton.setOnAction(e -> {
+            try {
+                switchToMain(e);
+            } catch (IOException ex) {throw new RuntimeException(ex);}
+        });
 
-    @FXML
-    private Button velocityBut;
+        categoriesBut.setOnAction(e -> {
+            try {
+                switchToCategories(e);
+            } catch (IOException ex) {throw new RuntimeException(ex);}
+        });
+    }
 
-    @FXML
-    private Button volumeBut;
+    private void switchToMain(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/filesFXML/mainPage-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
-    @FXML
-    private Button weightBut;
+    private void switchToCategories(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/filesFXML/categoriesPage-view.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
 }
--- src/main/java/com/example/unicalculator/controllers/MainPageController.java
+++ src/main/java/com/example/unicalculator/controllers/MainPageController.java
@@ -1,88 +1,42 @@
 package com.example.unicalculator.controllers;
 
+import com.example.unicalculator.MainApplication;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class MainPageController {
 
-    @FXML
-    private Button but0;
+    private Stage stage;
+    private Scene scene;
+    private Parent parent;
+    private double currentValue = 0;
+    private String currentOperation = "";
+    private StringBuilder expression = new StringBuilder();
+    private boolean isNewOperation = true;
 
     @FXML
-    private Button but1;
-
-    @FXML
-    private Button but2;
-
-    @FXML
-    private Button but3;
-
-    @FXML
-    private Button but4;
-
-    @FXML
-    private Button but5;
-
-    @FXML
-    private Button but6;
-
-    @FXML
-    private Button but7;
-
-    @FXML
-    private Button but8;
-
-    @FXML
-    private Button but9;
+    private Button but0, but1, but2, but3, but4, but5, but6, but7, but8, but9, calcButton, categoriesBut, clearBut
+            , commaBut, cosBut, convertBut, deleteBut, divBut, drBut, equalBut, expBut, historyBut, incBut
+            , lgBut, lnBut, percentBut, powBut, rootBut, sinBut, sndBut, subBut, sumBut;
 
     @FXML
     private GridPane buttonsGroup;
 
     @FXML
-    private Button calcButton;
-
-    @FXML
-    private Button categoriesBut;
-
-    @FXML
-    private Button clearBut;
-
-    @FXML
-    private Button commaBut;
-
-    @FXML
     private AnchorPane contentFrame;
-
-    @FXML
-    private Button cosBut;
-
-    @FXML
-    private Button convertBut;
-
-    @FXML
-    private Button deleteBut;
-
-    @FXML
-    private Button divBut;
-
-    @FXML
-    private Button drBut;
-
-    @FXML
-    private Button equalBut;
-
-    @FXML
-    private Button expBut;
-
-    @FXML
-    private Button historyBut;
-
-    @FXML
-    private Button incBut;
 
     @FXML
     private TextField inputField;
@@ -91,33 +45,140 @@ public class MainPageController {
     private AnchorPane inputFrame;
 
     @FXML
-    private Button lgBut;
-
-    @FXML
-    private Button lnBut;
-
-    @FXML
     private FlowPane mainBar;
 
     @FXML
     private AnchorPane mainWindow;
 
     @FXML
-    private Button percentBut;
+    private void initialize() {
+        categoriesBut.setOnAction(e -> {
+            try {
+                switchToCategories(e);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
-    @FXML
-    private Button powBut;
+        convertBut.setOnAction(e -> {
+            try {
+                switchToConverter(e);
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
 
-    @FXML
-    private Button rootBut;
+        Button[] buttons = {but0, but1, but2, but3, but4, but5, but6, but7, but8, but9};
+        for (Button button : buttons) {
+            button.setOnAction(e -> handleButtonPress(button));
+        }
 
-    @FXML
-    private Button sinBut;
+        deleteBut.setOnAction(e -> handleDeletePress());
+        clearBut.setOnAction(e -> handleClearPress());
+        sumBut.setOnAction(e -> handleOperationPress("+"));
+        subBut.setOnAction(e -> handleOperationPress("-"));
+        incBut.setOnAction(e -> handleOperationPress("*"));
+        divBut.setOnAction(e -> handleOperationPress("/"));
+        equalBut.setOnAction(e -> handleEqualsPress());
+    }
 
-    @FXML
-    private Button sndBut;
+    private void handleButtonPress(Button button) {
+        String buttonText = button.getText();
 
-    @FXML
-    private Button tanBut;
+        if (!(buttonText.equals("0") && (expression.length() == 0 || expression.toString().equals("0")))) {
+            expression.append(buttonText);
+            inputField.setText(expression.toString());
+        }
+    }
 
+    private void handleOperationPress(String operation) {
+        String currentText = inputField.getText();
+
+        if (!currentText.isEmpty() && !isLastCharOperator()) {
+            expression.append(operation);
+            inputField.setText(expression.toString());
+        }
+    }
+
+    private boolean isLastCharOperator() {
+        if (expression.length() > 0) {
+            char lastChar = expression.charAt(expression.length() - 1);
+            return lastChar == '+' || lastChar == '-' || lastChar == '*' || lastChar == '/';
+        }
+        return false;
+    }
+
+    private void handleEqualsPress() {
+        String currentText = inputField.getText();
+
+        if (!currentText.isEmpty() && !isLastCharOperator()) {
+            double result = evaluateExpression(expression.toString());
+
+            if (result % 1 == 0) {
+                inputField.setText(String.valueOf((int) result));
+            } else {
+                inputField.setText(String.valueOf(result));
+            }
+            expression.setLength(0);
+            expression.append(inputField.getText());
+        }
+    }
+
+    private double evaluateExpression(String expr) {
+        String[] tokens = expr.split("(?<=[-+*/])|(?=[-+*/])");
+        double result = Double.parseDouble(tokens[0]);
+
+        for (int i = 1; i < tokens.length; i += 2) {
+            String operator = tokens[i];
+            double operand = Double.parseDouble(tokens[i + 1]);
+
+            switch (operator) {
+                case "+":
+                    result += operand;
+                    break;
+                case "-":
+                    result -= operand;
+                    break;
+                case "*":
+                    result *= operand;
+                    break;
+                case "/":
+                    if (operand != 0) {
+                        result /= operand;
+                    } else {
+                        throw new ArithmeticException("Division by zero");
+                    }
+                    break;
+            }
+        }
+        return result;
+    }
+
+    private void handleClearPress() {
+        inputField.setText("");
+        expression.setLength(0);
+    }
+
+    private void handleDeletePress() {
+        if (expression.length() > 0) {
+            expression.setLength(expression.length() - 1);
+            inputField.setText(expression.toString());
+        }
+    }
+
+    private void switchToCategories(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/filesFXML/categoriesPage-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void switchToConverter(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/filesFXML/converterPage-view.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
